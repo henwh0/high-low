0b9acac2379f5739cd9bfa33da591b2153b87d99
henwh0 <hmcginnis96@gmail.com>
2025-12-23 23:01:05 -0500

    Add files via upload

---
+++ data/game_data.py
@@ -0,0 +1,62 @@
+data = [
+    {
+        "name": "John Cena",
+        "description": "Professional Wrestler, Actor",
+        "location": "United States",
+        "followers": 22000000
+    },
+    {
+        "name": "Iggy Azalea",
+        "description": "Singer",
+        "location": "Australia",
+        "followers": 17000000
+    },
+    {
+        "name": "Rihanna",
+        "description": "Singer, Designer",
+        "location": "Barbados",
+        "followers": 149000000
+    },
+    {
+        "name": "Emilia Clarke",
+        "description": "Actress",
+        "location": "United Kingdom",
+        "followers": 28000000
+    },
+    {
+        "name": "Matt Damon",
+        "description": "Actor",
+        "location": "United States",
+        "followers": 196000
+    },
+    {
+        "name": "Barack Obama",
+        "description": "44th United States President",
+        "location": "United States",
+        "followers": 40100000
+    },
+    {
+        "name": "Dua Lipa",
+        "description": "Musician",
+        "location": "United Kingdom",
+        "followers": 88700000
+    },
+    {
+        "name": "Shakira",
+        "description": "Musician",
+        "location": "Colombia",
+        "followers": 94300000
+    },
+    {
+        "name": "Gal Gadot",
+        "description": "Actress",
+        "location": "Israel",
+        "followers": 106000000
+    },
+    {
+        "name": "DJ Khaled",
+        "description": "DJ and producer",
+        "location": "United States",
+        "followers": 38900000
+    }
+]
